34174f36edc737276d0574a44719b5fb36ecbef9
Ahmedasif4774 <ahmedasif-55@hotmail.com>
2026-09-26 10:27:40 +0500

    Increase font size of search result title

--- components/en/Header.tsx
+++ components/en/Header.tsx
@@ -216,7 +216,7 @@ export default function Header({ dvHref }: { dvHref: string }) {
         }
         :global(.search-result-title) {
           font-family: "Archivo", sans-serif;
-          font-size: 13.5px;
+          font-size: 14.5px;
           font-weight: 600;
           line-height: 1.6;
           color: var(--ink);
